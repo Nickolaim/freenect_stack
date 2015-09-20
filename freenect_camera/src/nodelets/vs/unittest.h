e201b5cb724db3e1ec137dd138670c1121fded40
Nick Medveditskov <nickolaim@live.com>
2015-09-19 18:30:41 -0700

    Switch VS project to CPP Unit Test project from an application.

---
+++ freenect_camera/src/nodelets/vs/unittest.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#pragma warning (disable: 4505) // See https://support.microsoft.com/en-us/kb/947783 why disable/re-enable for this warning is not working properly.
+#include "CppUnitTest.h"
